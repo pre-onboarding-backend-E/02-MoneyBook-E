18bc68532d1c8c9ad75cb8674aa604d81397c276
LeoKim <leo3179@naver.com>
2022-07-08 17:16:36 +0900

    FIX / 가계부 서비스 버그 픽스
    - 가계부 서비스 수정
    - 가계부 서비스 테스트 파일 생성
    
    Relate to: [Task] TDD 생성 #3 [bug] 로그인 시 500 error 발생  #73

--- src/moneyBook/__test__/moneyBook.service.spec.ts
+++ src/moneyBook/__test__/moneyBook.service.spec.ts
@@ -1,41 +1,142 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { MoneyBookService } from './moneyBook.service';
-import { uuid } from 'uuidv4';
+import { Test } from '@nestjs/testing';
+import { async } from 'rxjs';
+import { User } from 'src/user/entities/user.entity';
 
-class MockMoneyBookRepositoy {
-  mydb = [
-    {
-      id: 'moneyBookId',
-      money: 1500,
-      type: '1',
-      total: 35000,
-      description: '새우깡 구매',
-      user: 'userId',
-    },
-  ];
+import { DataSource, QueryRunner, Repository } from 'typeorm';
+import { MoneyBook } from '../entities/moneyBook.entity';
+import { MoneyBookService } from '../moneyBook.service';
 
-  findOne({ id }) {
-    this.mydb.filter((el) => el.id === id);
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+const mockRepository = () => ({
+  find: jest.fn(),
+  findOne: jest.fn(),
+  save: jest.fn(),
+  softDelete: jest.fn(),
+});
+
+describe('MoneyBookService', () => {
+  let moneyBookService: MoneyBookService;
+  let moneyBookRepository: MockRepository<MoneyBook>;
+  let dataSource: DataSource;
+
+  const qr = {
+    manager: {},
+  } as QueryRunner;
+
+  class MockDataSource {
+    createQueryRunner(): QueryRunner {
+      return qr;
+    }
   }
-
-  save({ money, type, total, description }) {
-    const moneyBookId: string = uuid();
-    this.mydb.push({ id: moneyBookId, money, type, total, description });
-  }
-}
-
-describe('MoneyBooksService', () => {
-  let service: MoneyBookService;
+  qr.connect = jest.fn();
+  qr.startTransaction = jest.fn();
+  qr.commitTransaction = jest.fn();
+  qr.rollbackTransaction = jest.fn();
+  qr.release = jest.fn();
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MoneyBookService],
+    Object.assign(qr.manager, {
+      find: jest.fn(),
+      findOne: jest.fn(),
+      save: jest.fn(),
+      softDelete: jest.fn(),
+    });
+
+    const module = await Test.createTestingModule({
+      providers: [
+        MoneyBookService,
+        { provide: 'MoneyBookRepository', useFactory: mockRepository },
+        { provide: DataSource, useClass: MockDataSource },
+      ],
     }).compile();
 
-    service = module.get<MoneyBookService>(MoneyBookService);
+    moneyBookService = module.get<MoneyBookService>(MoneyBookService);
+    moneyBookRepository = module.get(
+      'MoneyBookRepository',
+    ) as MockRepository<MoneyBook>;
+    dataSource = module.get<DataSource>(DataSource);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('기본 테스트', () => {
+    expect(moneyBookService).toBeDefined();
   });
+
+  describe('getMoneyBook', () => {
+    it('가계부 내역 불러오기', async () => {
+      const bookId = 1;
+      const user: User = {
+        id: 1,
+        email: 'team01@naver.com',
+        password: '1234abcd',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        moneyBook: [],
+      };
+
+      moneyBookRepository.findOne.mockResolvedValueOnce({
+        id: 1,
+        money: 3000,
+        type: '1',
+        total: 15000,
+        description: '콜라 구매',
+      });
+
+      const result = await moneyBookService.getMoneyBook(bookId, user);
+
+      expect(result).toEqual({
+        id: 1,
+        money: 3000,
+        type: '1',
+        total: 15000,
+        description: '콜라 구매',
+      });
+    });
+  });
+
+  describe('latestMoneyBook', () => {
+    it('가계부 내역 중 가장 최신의 정보 불러오기', async () => {
+      const user: User = {
+        id: 1,
+        email: 'team01@naver.com',
+        password: '1234abcd',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        moneyBook: [],
+      };
+
+      moneyBookRepository.findOne.mockResolvedValueOnce([
+        {
+          id: 1,
+          money: 3000,
+          type: '1',
+          total: 15000,
+          description: '콜라 구매',
+        },
+      ]);
+
+      const result = await moneyBookService.latestMoneyBook(user);
+
+      expect(result).toEqual([
+        {
+          id: 1,
+          money: 3000,
+          type: '1',
+          total: 15000,
+          description: '콜라 구매',
+        },
+      ]);
+    });
+  });
+
+
+  describe('createMoneyBook',()=>{
+    it(){}
+  })
+
+
+
+
+
+
 });
