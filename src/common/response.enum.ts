c68575fcf5bcfd5017f0424f745c25e36d31a730
Yeom Ha neul <yeomhaneul@naver.com>
2022-07-08 09:16:06 +0900

    ADD / volta pin (버전 고정) [Task] package.json volta setting #57

--- src/common/response.enum.ts
+++ src/common/response.enum.ts
@@ -7,7 +7,7 @@ export const MSG = {
   createOne: { code: 201, msg: '가계부 생성에 성공했습니다!' },
   getAll: { code: 200, msg: '가계부 목록 조회에 성공했습니다!' },
   getOne: { code: 200, msg: '가계부 상세 조회에 성공했습니다!' },
-  modifyOne: { code: 200, msg: '가계부 수정 성공에 성공했습니다!' },
+  modifyOne: { code: 200, msg: '가계부 수정에 성공했습니다!' },
   deleteOne: { code: 200, msg: '가계부 삭제에 성공했습니다!' },
   restoreOne: { code: 200, msg: '가계부 복구에 성공했습니다!' },
 };
